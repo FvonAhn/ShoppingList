a61f3afeb5a3e1f32f079e9ba18eae8126d03f32
FvonAhn <fredd24hate@gmail.com>
2024-11-18 13:24:49 +0100

    Add project files.

---
+++ PracticeOne/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PracticeOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Shopping = new List<string>();
+            bool loopList = false;
+
+
+            Console.WriteLine("Välkommen till din Handlingslista");
+            Console.WriteLine("Ville du lägga till något?");
+§§
+            Console.ReadKey();
+        }
+    }
+}
