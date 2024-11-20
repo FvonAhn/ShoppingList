f8af24f3760e33c4ebb0fe5228ca57ec62b38f31
FvonAhn <fredd24hate@gmail.com>
2024-11-20 01:38:26 +0100

    Added some stuff eeeey.

--- PracticeOne/Program.cs
+++ PracticeOne/Program.cs
@@ -6,11 +6,33 @@ namespace PracticeOne
     {
         static void Main(string[] args)
         {
-            List<string> ShopList = new List<string>();
+            List<string> ShopList = [];
+            bool AddLoop = true;
 
             Console.WriteLine("Detta är din Handlingslista.");
+
+
             Console.WriteLine("Vill du lägga till något?");
-    
+            Console.WriteLine("Y / N");
+            string answer = Console.ReadLine()!;
+
+
+            if (answer == "y")
+            {
+                Console.WriteLine("Vad vill du lägga till?");
+                ShopList.Add(Console.ReadLine()!);
+            }
+            else
+            {
+                Console.WriteLine("OK, ingenting alltså..");
+            }
+
+
+            foreach (string item in ShopList)
+                Console.WriteLine($"Din lista innehåller: {item}");
+
+            Console.ReadKey();
         }
+            
     }
 }
