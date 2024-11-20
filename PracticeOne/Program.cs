fea791d9c749e52f03f793e381040704536a9a10
FvonAhn <fredd24hate@gmail.com>
2024-11-20 08:50:03 +0100

    Added a while loop over everything to get it to loop until the user is satisfied.

--- PracticeOne/Program.cs
+++ PracticeOne/Program.cs
@@ -8,25 +8,39 @@ namespace PracticeOne
         {
             List<string> ShopList = [];
             bool AddLoop = true;
+            bool qLoop = true;
 
             Console.WriteLine("Detta är din Handlingslista.");
 
-
-            Console.WriteLine("Vill du lägga till något?");
-            Console.WriteLine("Y / N");
-            string answer = Console.ReadLine()!;
-
-
-            if (answer == "y")
+            while (AddLoop)
             {
-                Console.WriteLine("Vad vill du lägga till?");
-                ShopList.Add(Console.ReadLine()!);
-            }
-            else
-            {
-                Console.WriteLine("OK, ingenting alltså..");
-            }
+                if (qLoop)
+                {
+                    Console.WriteLine("Vill du lägga till något?");
+                    qLoop = false;
+                }
+                else
+                {
+                    Console.WriteLine("Vill du lägga till något mer?");
+                }
+            
+                Console.WriteLine("Y / N");
+                string answer = Console.ReadLine()!;
 
+
+                if (answer == "y")
+                {
+                    Console.WriteLine("Vad vill du lägga till?");
+                    ShopList.Add(Console.ReadLine()!);
+                }
+            
+                else
+                {
+                    Console.WriteLine("Din handlingslista.");
+                    AddLoop = false;
+                }
+
+            }
 
             foreach (string item in ShopList)
                 Console.WriteLine($"Din lista innehåller: {item}");
