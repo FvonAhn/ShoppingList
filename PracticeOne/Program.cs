c78412499f7ebab831fb55e2ed8a84850ac53fb0
FvonAhn <fredd24hate@gmail.com>
2024-11-21 09:53:36 +0100

    Added some small details to make it more in line with my first vision ;)

--- PracticeOne/Program.cs
+++ PracticeOne/Program.cs
@@ -17,16 +17,16 @@ namespace PracticeOne
             {
                 if (qLoop) //this only changes the first question after the first time it is asked.
                 {
-                    Console.WriteLine("Vill du lägga till något?");
+                    Console.WriteLine("Vill du lägga till något? (Använd små bokstäver).");
                     qLoop = false;
                 }
                 else
                 {
-                    Console.WriteLine("Vill du lägga till något mer?"); //the altered question. 
+                    Console.WriteLine("Vill du lägga till något mer? (Anbänd dig av små bokstäver)"); //the altered question. 
                 }
             
                 Console.WriteLine("Y / N"); //a statement to make the user know what to answer.
-                string answer = Console.ReadLine()!;
+                string answer = Console.ReadLine()!.ToLower();
 
 
                 if (answer == "y")
@@ -34,17 +34,27 @@ namespace PracticeOne
                     Console.WriteLine("Vad vill du lägga till?"); //if the user answers "y" on the Q before, this questions appear.
                     ShopList.Add(Console.ReadLine()!);
                 }
-            
+                else if (answer == "n") 
+                {
+                    Console.WriteLine("Din handlingslista."); //if the user answers "no" on the Q the while-loop is cancelled and the shoppinglist appers with every item added.
+                    AddLoop = false;
+                }
                 else
                 {
-                    Console.WriteLine("Din handlingslista."); //if the user answers no on the Q the while-loop is cancelled and the shoppinglist appers with every item added.
-                    AddLoop = false;
+                    Console.WriteLine("Error, du kan enbart svara Ja(y) eller nej(n)på frågan."); 
                 }
 
             }
 
-            foreach (string item in ShopList)
-                Console.WriteLine($"Din lista innehåller: {item}");
+            if (ShopList.Count == 0)
+            {
+                Console.WriteLine("Din Handlingslisa är tom."); // this shows if the user dont add anything to the list.
+            }
+            else
+            { 
+                foreach (string item in ShopList)
+                    Console.WriteLine($"- {item}"); // and this writes out everything the user added to the list.
+            }
 
             Console.ReadKey();
         }
